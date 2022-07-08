d09cb8510440275f159c27a88967e57b8ee79d5c
nban-git <nban.works@gmail.com>
2022-07-08 08:00:07 -0700

    continuous commit

---
+++ leetcode/Q1632_RankTransformation.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def matrixRankTransform(self, matrix: List[List[int]]) -> List[List[int]]:
+        pass
+
+
+if __name__ == '__main__':
+    p = range(8)
+    for i in range(len(p)):
+        print(p[i])
+
+    p = [i for i in range(3)]
+    k = p[:]
+    print(k)
